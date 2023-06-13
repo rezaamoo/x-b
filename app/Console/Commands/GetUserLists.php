2a0a9b9bd82aaf4db68e04e1b59fc6a6394f1c7c
reza.hosseini181 <reza.hosseini181@gmail.com>
2023-06-13 21:56:07 +0330

    bug get users list fixed

--- app/Console/Commands/GetUserLists.php
+++ app/Console/Commands/GetUserLists.php
@@ -31,7 +31,7 @@ class GetUserLists extends Command
     public function handle()
     {
         $users = Http::retry(10, 1000)
-            ->post(config('v2board.base_url') . "/server/UniProxy/user", [
+            ->post(config('v2board.base_url') . "/server/UniProxy/allUsers", [
                 "token" => "nvFDdx8MvBXK4SduouKQEZ4xZD",
                 "node_type" => "v2ray",
                 "node_id" => "74"
