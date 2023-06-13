a4862c181d53026b8246cb3b86faae2c033365fb
reza.hosseini181 <reza.hosseini181@gmail.com>
2023-06-13 03:33:04 +0330

    added test

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $command = "sudo /usr/local/bin/xray api statsquery --server=127.0.0.1:8080 -pattern '' -reset";
+        $output = shell_exec($command);
+
+        Log::info(json_encode($output));
+    }
+}
